14acaa153b4800e32e1af38ca4ee67f506e8297e
Maxime Salomon <maxime@croissant.io>
2019-05-16 18:29:01 +0200

    Create AH starter kit

--- app.js
+++ app.js
@@ -1,95 +1,58 @@
-const trash = "https://image.flaticon.com/icons/svg/1214/1214428.svg"
+const trash = "https://image.flaticon.com/icons/svg/1214/1214428.svg";
 
-document.getElementById('add-task').addEventListener('click', function() {
-    let taskValue = document.getElementById('task-value').value;
-    if (taskValue) addTask(taskValue);
-    document.getElementById('task-value').value = '';
-});
-
-const addTask = (taskValue) => {
-    let task = document.createElement('li');
-    task.classList.add('task');
-    task.classList.add('fill');
-    task.setAttribute("draggable", "true");
-    task.addEventListener('dragstart', dragStart);
-    task.addEventListener('dragend', dragEnd);
-
-    let taskContent = document.createElement('div');
-    taskContent.classList.add('task-content');
-    taskContent.innerText = taskValue;
-    
-    let trash = document.createElement('div');
-    trash.classList.add('trash');
-    trash.innerText = "X";
-    trash.addEventListener('click', removeTask);
-
-    task.appendChild(taskContent);
-    task.appendChild(trash);
-
-    let tasks = document.getElementById('tasks-added');
-    tasks.insertBefore(task, tasks.childNodes[0]);
-}
-
-const removeTask = (event) => {
-    let tasks = event.target.parentNode.parentNode;
-    let task = event.target.parentNode;
-    tasks.removeChild(task);
-}
+// Add an event listener to the #add-task input field
+// Only addTask if there is text in the input
+// Remove the task text in the input once its added
 
 
-// DRAG & DROP
 
-let task
 
-const dragStart = (event) => {
-    // console.log(event.target);
-    event.target.className += ' hold';
-    task = event.target;
-    setTimeout(() => (event.target.className = 'invisible'), 0);
-}
+////// TASK FUNCTIONS
 
-const dragEnd = (event) => {    
-    // console.log(event.target);
-    event.target.className = 'task fill';
-}
+// Create a function addTask
 
-const dropzones = document.querySelectorAll('.dropzone');
 
-const dragEnter = (event) => {
-    // console.log("ENTER");
-    event.preventDefault();
-    if(event.target.className === "column dropzone") {
-        event.target.className += ' hovered';   
-    }
-}
 
-const dragOver = (event) => {
-    // console.log("OVER");
-    event.preventDefault();
-}
 
-const dragLeave = (event) => {
-    // console.log("LEAVE");
-    if(event.target.className === "column dropzone hovered") {
-        event.target.className = "column dropzone"
-    }
-}
+// Create a function removeTask
 
-const dragDrop = (event) => {
-    // console.log("DROP");
-    if(event.target.className === "column dropzone hovered") {
-        event.target.className = "column dropzone"
-    }
-    event.target.append(task);
-}
 
-for(const dropzone of dropzones) {
-    dropzone.addEventListener('dragenter', dragEnter);
-    dropzone.addEventListener('dragover', dragOver);
-    dropzone.addEventListener('dragleave', dragLeave);
-    dropzone.addEventListener('drop', dragDrop);
-}
 
+
+////// DRAG & DROP
+
+// Create a variable task to store the selected task
+
+
+// Add an event listener dragstart to task
+
+
+
+// Create a dragStart function
+
+
+// Create a dragEnd function
+
+
+// Create dropzones by selecting .dropzone
+
+
+// Add eventlisteners to each dropzone
+
+
+// Create a function dragEnter
+
+
+// Create a function dragOver
+
+
+// Create a function dragLeave
+
+
+// Create a function dragLeave
+
+
+// Create a function dragDrop
 
 
 
